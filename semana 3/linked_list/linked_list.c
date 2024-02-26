2b80e04e6f693c223525e490a645563acee2872d
Giulia Machado <60783184+GiuliaMachado@users.noreply.github.com>
2024-02-26 14:46:26 +0000

    linked list

---
+++ semana 3/linked_list/linked_list.c
@@ -0,0 +1,8 @@
+#include <stdbool.h>
+#include "linked_list.h"
+
+bool list_removeAt(LinkedList* list, int pos){
+    if(pos <0 || pos >= list){
+        
+    }
+}
---
+++ semana 3/linked_list/linked_list.h
@@ -0,0 +1,6 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+typedef struct LinkedList LinkedList;
+bool list_removeAt(LinkedList* list, int pos);
+#endif
