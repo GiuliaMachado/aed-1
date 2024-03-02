1b5c92b9957f68e57db5142e9626e3895b31654d
Giulia Machado <60783184+GiuliaMachado@users.noreply.github.com>
2024-03-02 16:45:44 +0000

    linked list operacoes

--- semana_3/linked_list/linked_list.c
+++ semana_3/linked_list/linked_list.c
@@ -1,8 +1,87 @@
-#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "linked_list.h"
 
+//definir o NODE e LinkedList com ponteiro pra cabeça
+typedef struct Node {
+    Element element;
+    struct Node* next;
+}Node;
+
+struct LinkedList{
+    Node* head;
+    int size;
+};
+
+LinkedList* list_create(){
+    LinkedList* list = malloc(sizeof(LinkedList));
+
+    list->head = NULL;
+    list->size =0;
+
+    return list;
+}
+
+void list_destroy(LinkedList* list){
+    Node* cur = list->head;
+
+    while(cur != NULL){
+        Node* tresh = cur;
+        cur = cur->next;
+        free(tresh);
+    }
+    free(list);
+}
+
+void list_insertFirst(LinkedList* list, Element e){
+    //criamos o novo nó
+    Node* newNode = malloc(sizeof(Node));
+    //passamos o elemento do nó
+    newNode->element = e;
+    //o ponteiro pro proximo nó é a cabeça da lista
+    newNode->next = list->head;
+    //a cabeça da lista recebe o conteudo do novo nó
+    list->head = newNode;
+    list->size++;
+}
+
+void list_insertAfter(LinkedList* list, int pos, Element e){
+    //caso 1: inserir no começo
+    if(list->head == NULL || pos < 0){
+        list_insertFirst(list, e);
+    }
+    else{
+        Node* cur = list->head;
+        int i = 0;
+
+        while(i < pos && cur->next != NULL){
+            cur = cur->next;
+            i++;
+        }
+
+        Node* newNode = malloc(sizeof(Node));
+        newNode->element = e;
+        newNode->next = cur->next;
+        cur->next = newNode;
+        list->size++;
+
+    }
+}
+bool list_removeFirst(LinkedList* list){
+    
+}
 bool list_removeAt(LinkedList* list, int pos){
     if(pos <0 || pos >= list){
         
     }
+}
+
+void list_print(LinkedList* list){
+    Node* cur = list->head;
+
+    while(cur != NULL){
+        element_print(&cur->element);
+        cur = cur->next;
+    }
+    printf("\n");
 }
--- semana_3/linked_list/linked_list.h
+++ semana_3/linked_list/linked_list.h
@@ -1,10 +1,23 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+#include <stdbool.h>
+#include "element.h"
+
 typedef struct LinkedList LinkedList;
 
 LinkedList* list_create();
 void list_destroy(LinkedList* list);
-
+//inserção tem 3 tipos: 1- NO COMEÇO/ 2- NO FINAL/ 3- NO MEIO
+void list_insertFirst(LinkedList* list, Element e);
+void list_insertAfter(LinkedList* list, int pos, Element e);
+//remoção
+bool list_removeFirst(LinkedList* list);
 bool list_removeAt(LinkedList* list, int pos);
+bool list_remove(LinkedList* list, int pos, Element e);
+//pegar elementos
+int list_get(LinkedList* list, int pos);
+int list_size(LinkedList* list);
+int list_isEmpty(LinkedList* list);
+void list_print(LinkedList* list);
 #endif
